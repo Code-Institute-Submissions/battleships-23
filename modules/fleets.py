b5b70a9de43502e7bdfb2e249a6d9fd6725adfff
ianmeigh <ianmeigh@googlemail.com>
2022-02-28 16:34:38 +0000

    feat: Add basic structure for fleets class

---
+++ modules/fleets.py
@@ -0,0 +1,26 @@
+from ships import Battleship, Submarine
+
+
+class Fleet:
+    """Class to held metods for fleets"""
+
+    def get_ships_in_fleet(self):
+        return self.fleet
+
+
+class SmallFleet(Fleet):
+    """
+    Class to represent a small collection of ships
+    """
+
+    def __init__(self):
+        pass
+
+
+class LargeFleet(Fleet):
+    """
+    Class to represent a large collection of ships
+    """
+
+    def __init__(self):
+        pass
