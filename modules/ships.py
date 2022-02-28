1331da4718a7ca99c67d6349e8b4635e4804ad63
ianmeigh <ianmeigh@googlemail.com>
2022-02-28 15:32:43 +0000

    feat: Add instance variables and Ship subclasses

--- modules/ships.py
+++ modules/ships.py
@@ -1,3 +1,26 @@
 class Ship:
+    def __init__(self, name, symbol, length):
+        self.name = name
+        self.symbol = symbol
+        self.length = length
+
+    def get_symbol(self):
+        return self.symbol
+
+    def get_name(self):
+        return self.name
+
+
+class Battleship(Ship):
     def __init__(self):
-        pass
+        super().__init__("Battleship", "B", 4)
+
+
+class Submarine(Ship):
+    def __init__(self):
+        super().__init__("Submarine", "S", 2)
+
+
+sub = Submarine()
+print(sub.__dict__)
+help(sub)
