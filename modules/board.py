2464dfe2a8f1ba38f6e7dc34cd8cdcea0428966f
ianmeigh <ianmeigh@googlemail.com>
2022-02-28 13:45:18 +0000

    feat: Add basic structure to board class

---
+++ modules/board.py
@@ -0,0 +1,13 @@
+class Board:
+    """
+    Class to represent the game board
+    """
+
+    def __init__(self):
+        pass
+
+    def create_board():
+        pass
+
+    def print_boards():
+        pass
