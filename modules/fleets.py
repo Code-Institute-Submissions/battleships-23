978bf30b423ad548408a735b5eb83d5ff6efc874
ianmeigh <ianmeigh@googlemail.com>
2022-02-28 16:44:23 +0000

    feat: Populate SmallFleet for use when placing ships

--- modules/fleets.py
+++ modules/fleets.py
@@ -4,17 +4,31 @@ from ships import Battleship, Submarine
 class Fleet:
     """Class to held metods for fleets"""
 
+    def __init__(self):
+        self.fleet = None
+
     def get_ships_in_fleet(self):
+        """Returns all the ship objects from the fleet
+
+        Returns:
+            list: List of Ship objects
+        """
         return self.fleet
 
 
 class SmallFleet(Fleet):
     """
     Class to represent a small collection of ships
+
+    1 x Battleship
+    2 x Submarines
     """
 
     def __init__(self):
-        pass
+        self.ship1 = Battleship()
+        self.ship2 = Submarine()
+        self.ship3 = Submarine()
+        self.fleet = [self.ship1, self.ship2, self.ship3]
 
 
 class LargeFleet(Fleet):
@@ -22,5 +36,12 @@ class LargeFleet(Fleet):
     Class to represent a large collection of ships
     """
 
+    # TODO Implement large fleet
+
     def __init__(self):
         pass
+
+
+new_test_small_fleet = SmallFleet()
+ships_in_fleet = new_test_small_fleet.get_ships_in_fleet()
+print(ships_in_fleet)
