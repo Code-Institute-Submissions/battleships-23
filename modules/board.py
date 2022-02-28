4e68b1155e7d410e5c10342adf8dc876c99fc368
ianmeigh <ianmeigh@googlemail.com>
2022-02-28 14:51:11 +0000

    feat: Add functionality to print boards side by side
    The play_board (that will hold the players board with ship placements)
    and the guess_board (that will hold the players guess results) are
    printed side by side for reference as in a physical game of Battleships.

--- modules/board.py
+++ modules/board.py
@@ -6,8 +6,8 @@ class Board:
     def __init__(self, size):
         # Idea to use 2 boards per player, 1 to hold ships and 1 to hold
         # guesses from David Bowers project.
-        # CREDIT: David Bowers URL:
-        # https://github.com/dnlbowers/battleships
+        # CREDIT: David Bowers
+        # URL: https://github.com/dnlbowers/battleships
         self.play_board = Board.create_board(size)
         self.guess_board = Board.create_board(size)
 
@@ -24,8 +24,36 @@ class Board:
 
     def print_board(self):
         """Prints each row of the board on a separate line in the console"""
-        for row in self.play_board:
-            print(row)
+        row_num = 0
+
+        # Idea to use print boards next to each other from David Bowers
+        # project.
+        # CREDIT: David Bowers
+        # URL: https://github.com/dnlbowers/battleships
+        combined_boards = list(zip(self.play_board, self.guess_board))
+
+        # Code to use chars the label y axis
+        # CREDIT: Pythondex Tutorial
+        # URL: https://pythondex.com/python-battleship-game
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        alphabet = alphabet[0 : len(combined_boards) + 1]
+
+        for i in range(2):
+            print("    ", end="")
+            for column_num in range(1, len(combined_boards) + 1):
+                print(f" {column_num}  ", end="")
+            print("\t", end="")
+        for combined_row in combined_boards:
+            print("")
+            for row in combined_row:
+                print(f"{alphabet[row_num]}) |", end="")
+                for item in row:
+                    # print(item)
+                    if item is None:
+                        print(" - |", end="")
+                print("\t", end="")
+            row_num += 1
+        print("\n")
 
 
 new_test_board = Board(5)
