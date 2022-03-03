43ebf5ae8e1ef1c68fa8326fdb141afd6dd92cf0
ianmeigh <ianmeigh@googlemail.com>
2022-03-03 20:25:40 +0000

    Initial commit

---
+++ modules/player.py
@@ -0,0 +1,19 @@
+class Player:
+    def __init__(self):
+        pass
+
+
+class HumanPlayer(Player):
+    def __init__(self):
+        super().__init__()
+
+    def place_ships(self):
+        pass
+
+
+class ComputerPlayer(Player):
+    def __init__(self):
+        super().__init__()
+
+    def place_ship():
+        pass
