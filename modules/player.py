df0b2c60115125671a2128432693b045fbdf33db
ianmeigh <ianmeigh@googlemail.com>
2022-03-03 20:32:21 +0000

    feat: Add basic structure for the Player class

--- modules/player.py
+++ modules/player.py
@@ -7,13 +7,7 @@ class HumanPlayer(Player):
     def __init__(self):
         super().__init__()
 
-    def place_ships(self):
-        pass
-
 
 class ComputerPlayer(Player):
     def __init__(self):
         super().__init__()
-
-    def place_ship():
-        pass
