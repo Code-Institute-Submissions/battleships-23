d4532399aec21b30f71f3f121a3b855dccf9b277
ianmeigh <ianmeigh@googlemail.com>
2022-03-11 21:03:05 +0000

    feat: Add large fleet and adjust ship properties

--- battleships/fleets.py
+++ battleships/fleets.py
@@ -1,4 +1,4 @@
-from .ships import Battleship, Submarine
+from .ships import *
 
 
 class Fleet:
@@ -24,25 +24,46 @@ class Fleet:
 
 class SmallFleet(Fleet):
     """
-    Class to represent a small collection of ships
+    Class to represent a small fleet of ships
 
     1 x Battleship
     2 x Submarines
+    1 x PatrolBoat
     """
 
     def __init__(self):
         self.ship1 = Battleship()
         self.ship2 = Submarine()
         self.ship3 = Submarine()
-        self.fleet = [self.ship1, self.ship2, self.ship3]
+        self.ship4 = PatrolBoat()
+        self.fleet = [self.ship1, self.ship2, self.ship3, self.ship4]
 
 
 class LargeFleet(Fleet):
     """
-    Class to represent a large collection of ships
+    Class to represent a large fleet of ships
+
+    1 x Aircraft Carrier
+    1 x Battleship
+    2 x Cruisers
+    2 x Submarines
+    1 x PatrolBoat
     """
 
-    # TODO Implement large fleet
-
     def __init__(self):
-        pass
+        self.ship1 = AircraftCarrier()
+        self.ship2 = Battleship()
+        self.ship3 = Cruiser()
+        self.ship4 = Cruiser()
+        self.ship5 = Submarine()
+        self.ship6 = Submarine()
+        self.ship7 = PatrolBoat()
+        self.fleet = [
+            self.ship1,
+            self.ship2,
+            self.ship3,
+            self.ship4,
+            self.ship5,
+            self.ship6,
+            self.ship7,
+        ]
