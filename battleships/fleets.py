c1ef23d4ca3d613fa18c1e7751f84af57c32b0f2
ianmeigh <ianmeigh@googlemail.com>
2022-03-16 17:24:47 +0000

    refactor: Add comments and remove duplicate functionality

--- battleships/fleets.py
+++ battleships/fleets.py
@@ -1,32 +1,59 @@
+"""
+Creates a fleet of ships imported from the ships module
+
+Classes:
+
+    Fleet,
+    Small_Fleet,
+    Large_Fleet
+"""
 from .ships import *
 
 
 class Fleet:
-    """Class to held metods for fleets"""
+    """
+    A Class to represent a fleet
+
+
+    Attributes
+    ----------
+    fleet : None
+        Variable to contain a fleet
+
+    Methods
+    -------
+    get_ships_in_fleet():
+        Returns the fleet variable
+
+    get_num_ships_remaining():
+        Returns the number of ship objects in fleet still afloat
+    """
 
     def __init__(self):
+        """
+        Constructs all the necessary attributes for a fleet
+        """
         self.fleet = None
 
     def get_ships_in_fleet(self):
-        """Returns all the ship objects from the fleet
+        """Gets all ship objects in fleet
 
         Returns:
             list: List of Ship objects
         """
         return self.fleet
 
-    def ships_remaining(self):
+    def get_num_ships_remaining(self):
+        """Gets the number of ship objects in fleet still afloat
+
+        Returns:
+            int: Number of Ship objects
+        """
         number_of_ships = 0
         for ship in self.fleet:
             if ship.get_floatation_status() is True:
                 number_of_ships += 1
         return number_of_ships
-
-    def is_fleet_destroyed(self):
-        for ship in self.fleet:
-            if ship.get_floatation_status() is True:
-                return False
-        return True
 
 
 class SmallFleet(Fleet):
@@ -39,6 +66,9 @@ class SmallFleet(Fleet):
     """
 
     def __init__(self):
+        """
+        Constructs all the necessary attributes for a small fleet
+        """
         self.ship1 = Battleship()
         self.ship2 = Submarine()
         self.ship3 = Submarine()
@@ -58,6 +88,9 @@ class LargeFleet(Fleet):
     """
 
     def __init__(self):
+        """
+        Constructs all the necessary attributes for a large fleet
+        """
         self.ship1 = AircraftCarrier()
         self.ship2 = Battleship()
         self.ship3 = Cruiser()
